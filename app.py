f3f5772a99541d68c7f9541e7a29c1fdeee3009d
Peluzza11 <ro89585@gmail.com>
2024-01-20 11:10:45 -0800

    app.py

--- app.py
+++ app.py
@@ -1,2 +1,8 @@
-from petfax import create_app
-app = create_app()
+# config                    
+from flask import Flask
+app = Flask(__name__)
+
+# index route
+@app.route('/')
+def index(): 
+    return 'Hello, this is PetFax!'
